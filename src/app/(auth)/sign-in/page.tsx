c2aa1abf328ad7cf1592569024a9c7bb99063f8a
amanpalap <pal90589500@gmail.com>
2024-07-17 01:42:42 +0530

    update .env, page.tsx, layout.tsx and middleware.ts

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -14,7 +14,7 @@ export default function Component() {
     return (
         <>
             Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
+            <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={() => signIn()}>Sign in</button>
         </>
     )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,17 +7,21 @@ export async function middleware(request: NextRequest) {
 
     const token = await getToken({ req: request })
     const url = request.nextUrl
-    if (!token && (
+
+    if (token && (
         url.pathname.startsWith('/sign-in') ||
         url.pathname.startsWith('/sign-up') ||
         url.pathname.startsWith('/verify') ||
-        url.pathname === '/'
-    )) {
+        url.pathname.startsWith('/')
+    )
+    ) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-    if(!token && url.pathname.startsWith('/dashboard')) {
+    if (!token && url.pathname.startsWith('/dashboard')) {
         return NextResponse.redirect(new URL('/sign-in', request.url))
     }
+
+    return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
